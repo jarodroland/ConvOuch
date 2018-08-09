ce4c34e17fb5baffa25a302cd2152a344bc289b7
Jarod Roland <jarod.roland@gmail.com>
2018-08-09 14:17:06 -0700

    added DataGenerator functionality

--- Run_Imgenet_VGG16_Model.py
+++ Run_Imgenet_VGG16_Model.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import re
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -7,21 +9,56 @@ from keras.models import Sequential
 from keras.models import Model
 from keras.layers import Dense
 from keras.layers import Conv2D
-
 from keras.layers import MaxPool2D
 from keras.layers import Flatten
-
 from keras.optimizers import Adam
-
 from keras.layers import Dropout
-
 from keras.applications.vgg16 import VGG16
 from keras.utils.vis_utils import plot_model
-
 from keras.applications.vgg16 import decode_predictions
 
+from CQ500DataGenerator import DataGenerator
 
-num_train_set = 40
+# define our variables
+data_dir = '/Volumes/My4TB/CQ500/Data/'
+num_slices_per_subject = 28       # always using 28 slices per subject
+
+# create list of IDs from all slices
+all_IDs = set()
+all_Slices = glob.glob(data_dir + "Slices/CQ500-CT-*")
+for item in all_Slices:
+    subj_match = re.match(data_dir + "Slices/(CQ500-CT-[0-9]+)_Slice[0-9]+.npy", item)
+    subj_id = subj_match.group(1)
+    all_IDs = all_IDs.union([subj_id])
+
+all_IDs_slices = list()
+for subj_id in all_IDs:
+    for slice_num in range(num_slices_per_subject):
+        all_IDs_slices.append(subj_id + "_Slice" + str(slice_num))
+
+
+# # create a dict of labels for all slices
+# all_labels = dict()
+# label_files = glob.glob(data_dir + "Labels/CQ500-CT-*")
+# for item in label_files:
+#     slice_match = re.match(data_dir + "Labels/(CQ500-CT-[0-9]+)_Slice([0-9]+).npy", item)
+#     subj_id = slice_match.group(1)
+#     slice_num = slice_match.group(2)
+#     # data_obj = np.load(item)
+#     data_dict = data_obj.item()
+#     all_labels[subj_id + "_Slice" + slice_num] = int(data_dict["label"])    # store labes as 1 or 0 for True or False
+
+
+# divide list into train and validation
+percentage_to_train = 0.8
+cutoff_index = int(np.floor(len(all_IDs) * percentage_to_train)) * num_slices_per_subject
+training_IDs = all_IDs_slices[0:cutoff_index]
+validation_IDs  = all_IDs_slices[cutoff_index:]
+
+training_generator = DataGenerator(training_IDs)
+validation_generator = DataGenerator(validation_IDs)
+
+# num_train_set = 40
 
 
 #add our own fully connected layers for the final classification
@@ -31,7 +68,7 @@ base_model = VGG16(include_top=False, weights='imagenet',
                     input_tensor=None, input_shape=(224, 224, 3), pooling=None)
 
 x = base_model.output
-#flattentit
+#flatten it
 x = Flatten()(x)
 # let's add a fully-connected layer
 x = Dense(4096, activation='relu')(x)
@@ -51,16 +88,8 @@ for layer in base_model.layers:
     
 # compile the model (should be done *after* setting layers to non-trainable)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit_generator(generator=training_generator, validation_data=validation_generator, use_multiprocessing=False)
 
+# train_img, train_label, validation_split=0.1, epochs=1, batch_size=5)    
+# model.save('FirstModel.h5')
 
-for i in range(0, num_train_set):
-    train_img_file = 'Train/train' + str(i+1) + '_img.npy'
-    train_label_file = 'Train/train' + str(i+1) + '_label.npy'
-    
-    train_img = np.load(train_img_file)
-    train_label = np.load(train_label_file)
-
-
-    model.fit(train_img, train_label, validation_split=0.1, epochs=1, batch_size=5)
-    
-model.save('FirstModel.h5')
